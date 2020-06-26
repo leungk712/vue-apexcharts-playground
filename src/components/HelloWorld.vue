204a68757d7360945e84512f093dfe0e3ba62cca
Kevin Leung <kleung@MacBook-Pro.local>
2020-06-25 21:30:54 -0600

    install apexcharts, creat 4 current charts testing

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,130 +1,294 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-          >babel</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router"
-          target="_blank"
-          rel="noopener"
-          >router</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex"
-          target="_blank"
-          rel="noopener"
-          >vuex</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
-          target="_blank"
-          rel="noopener"
-          >eslint</a
-        >
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
-  </div>
+  <v-container>
+    <v-row class="text-center">
+      <v-col cols="6">
+        <v-card>
+          <v-card-title>
+            Basic Bar Graph
+          </v-card-title>
+          <v-card-text>
+            <apexchart
+              width="100%"
+              :options="barOptions"
+              :series="barOptions.series"
+            />
+          </v-card-text>
+        </v-card>
+      </v-col>
+
+      <v-col cols="6">
+        <v-card>
+          <v-card-title>
+            Basic Line Graph
+          </v-card-title>
+          <v-card-text>
+            <apexchart
+              width="100%"
+              :options="lineOptions"
+              :series="lineOptions.series"
+            />
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+
+    <v-row>
+      <v-col cols="6">
+        <v-card>
+          <v-card-title>
+            Basic Donut
+          </v-card-title>
+          <v-card-text>
+            <apexchart
+              width="100%"
+              :options="donutOptions"
+              :series="donutOptions.series"
+            />
+          </v-card-text>
+        </v-card>
+      </v-col>
+
+      <v-col cols="6">
+        <v-card>
+          <v-card-title>
+            Basic Heatmap
+          </v-card-title>
+          <v-card-text>
+            <apexchart
+              width="100%"
+              :options="heatOptions"
+              :series="heatOptions.series"
+            />
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
 export default {
   name: "HelloWorld",
-  props: {
-    msg: String
+
+  data() {
+    return {
+      barOptions: {
+        series: [
+          {
+            name: "yearly revenue",
+            data: [
+              300000,
+              450000,
+              525000,
+              815000,
+              938414,
+              1265783,
+              1567892,
+              1777123,
+              1923400
+            ]
+          }
+        ],
+        chart: {
+          id: "revenue-yearly",
+          type: "bar"
+        },
+        plotOptions: {
+          bar: {
+            horizontal: true
+          }
+        },
+        xaxis: {
+          categories: [
+            2016,
+            2017,
+            2018,
+            2019,
+            2020,
+            2021,
+            2022,
+            2023,
+            2024,
+            2025
+          ]
+        }
+      },
+
+      lineOptions: {
+        series: [
+          {
+            name: "yearly revenue",
+            data: [
+              300000,
+              450000,
+              525000,
+              815000,
+              938414,
+              1265783,
+              1567892,
+              1777123,
+              1923400
+            ]
+          }
+        ],
+        chart: {
+          id: "revenue-yearly",
+          type: "line"
+        },
+        xaxis: {
+          categories: [
+            2016,
+            2017,
+            2018,
+            2019,
+            2020,
+            2021,
+            2022,
+            2023,
+            2024,
+            2025
+          ]
+        }
+      },
+
+      donutOptions: {
+        series: [33, 17, 48, 63, 88, 71],
+        chart: {
+          type: "donut"
+        },
+        labels: [
+          "Groceries",
+          "Supplies",
+          "Clothes",
+          "Bills",
+          "Misc 1",
+          "Misc 2"
+        ],
+        responsive: [
+          {
+            breakpoint: 480,
+            options: {
+              chart: {
+                width: 200
+              },
+              legend: {
+                position: "bottom"
+              }
+            }
+          }
+        ]
+      },
+
+      heatOptions: {
+        series: [
+          {
+            name: "Series 1",
+            data: [
+              {
+                x: "W1",
+                y: 22
+              },
+              {
+                x: "W2",
+                y: 29
+              },
+              {
+                x: "W3",
+                y: 13
+              },
+              {
+                x: "W4",
+                y: 32
+              }
+            ]
+          },
+          {
+            name: "Series 2",
+            data: [
+              {
+                x: "W1",
+                y: 50
+              },
+              {
+                x: "W2",
+                y: 88
+              },
+              {
+                x: "W3",
+                y: 1
+              },
+              {
+                x: "W4",
+                y: 9
+              }
+            ]
+          },
+          {
+            name: "Series 3",
+            data: [
+              {
+                x: "W1",
+                y: 45
+              },
+              {
+                x: "W2",
+                y: 53
+              },
+              {
+                x: "W3",
+                y: 78
+              },
+              {
+                x: "W4",
+                y: 88
+              }
+            ]
+          }
+        ],
+        chart: {
+          height: 350,
+          type: "heatmap"
+        },
+        dataLabels: {
+          enabled: false
+        },
+        plotOptions: {
+          heatmap: {
+            colorScale: {
+              ranges: [
+                {
+                  from: 0,
+                  to: 25,
+                  color: "#CACFD2",
+                  name: "low"
+                },
+                {
+                  from: 26,
+                  to: 50,
+                  color: "#C0392B",
+                  name: "medium"
+                },
+                {
+                  from: 51,
+                  to: 75,
+                  color: "#F5B041",
+                  name: "medium-high"
+                },
+                {
+                  from: 76,
+                  to: 100,
+                  color: "#52BE80",
+                  name: "high"
+                }
+              ]
+            }
+          }
+        },
+        title: {
+          text: "HeatMap Chart (Single color)"
+        }
+      }
+    };
   }
 };
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
